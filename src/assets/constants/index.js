b8df317482e130465ef7927b464a4638289fdd83
Abdi-01 <abdialghi@gmail.com>
2022-05-27 19:44:44 +0700

    fix API_URL

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,3 +1,3 @@
 export const API_URL = `https://jcwdvl00402api.purwadhikabootcamp.com`;
 
-export const SOCKET_URL = `http://localhost:7000`;
+export const SOCKET_URL = `https://jcwdvl00402socket.purwadhikabootcamp.com`;
